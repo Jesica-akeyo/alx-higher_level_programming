862d6172057cadd8dc9e33f84dfe4ec7788d3a99
Jesica-akeyo <Jessica.akeyo@gmail.com>
2022-06-02 10:53:47 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,8 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments.")
     elif len(argv) == 2:
-        print("{:d} argument: ".format(len(argv) - 1))
+        print("1 argument:\n1: {}".format(argv[1]))
     else:
-        print("{:d} arguments: ".format(len(argv) - 1))
-
-    for i, arg in enumerate(argv[1:], start=1):
-        print("{:d}: {:s}".format(i, arg))
+        print("{:d} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{:d}: {}".format(i, argv[i]))
