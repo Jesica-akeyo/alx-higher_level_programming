9fb8ec58ab203bd043ac3e52800af55e0445ab98
Jesica-akeyo <Jessica.akeyo@gmail.com>
2022-06-28 15:25:00 +0000

    corrections

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l += ["BestSchool"]
-    return ", ".join(l)
+def magic_string(mgc_str=[]):
+    mgc_str += ["BestSchool"]
+    return ", ".join(mgc_str)
