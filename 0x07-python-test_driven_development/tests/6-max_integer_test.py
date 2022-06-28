f2cd215c5958ce8699341204929f8208cfac9f3a
Jesica-akeyo <99292017+Jesica-akeyo@users.noreply.github.com>
2022-06-28 22:40:42 +0300

    update task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,22 +5,60 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
-class testlist(unittest.TestCase):
-    """
-    This class contains tests for the function max_integer in 6-max_integer.py
-    """
-    def test_max(self):
-        """ method that holds the tests
-        """
-        self.assertEqual(max_integer([1, 2, 3]), 3)
-        self.assertEqual(max_integer([10, 2, 3]), 10)
-        self.assertEqual(max_integer([1, 1, 0, -3]), 1)
-        self.assertEqual(max_integer([5, 7, 9, 8, 9, 1]), 9)
-        self.assertEqual(max_integer("helloez"), "z")
-        self.assertEqual(max_integer([[5, 0], [4, 20]]), [5, 0])
-        self.assertEqual(max_integer([1]), 1)
-        self.assertEqual(max_integer(""), None)
+class TestMaxInteger(unittest.TestCase):
+
+    """Suite test for max_integer function"""
+
+    def test_max_integer(self):
+        self.assertEqual(max_integer([5, -2, 100, 3]), 100)
+
+    def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer(), None)
+
+    def test_repeated_number(self):
+        self.assertEqual(max_integer([1000, 1000, 1000]), 1000)
+
+    def test_float_numbers(self):
+        self.assertEqual(max_integer([50, 51, 50, 49]), 51)
+
+    def test_max_operated_integer(self):
+        self.assertEqual(max_integer([-3, -5 * -5, 12, -1]), 25)
+
+    def test_neg_numbers(self):
+        self.assertEqual(max_integer([-10, -5, -2, -1]), -1)
+
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
+
+    def test_zero_number(self):
+        self.assertEqual(max_integer([0, 0]), 0)
+
+    def test_big_list(self):
+        self.assertEqual(max_integer([
+            901, 902, 903, 904, 905, 906, 907, 908, 909, 910,
+            911, 912, 913, 914, 915, 916, 917, 918, 919, 920,
+            919, 918, 917, 1000, 915, 914, 913, 912, 911, 910,
+            909, 908, 907, 906, 905, 904, 903, 902, 901]), 1000)
+
+    def test_list_with_loop(self):
+        my_list = [1, 2, 3, 4, 5]
+        self.assertEqual(max_integer([i * 5 for i in my_list]), 25)
+
+    def test_one_number_in_a_list(self):
+        self.assertEqual(max_integer([1]), 1)
+
+    def test_string_number_in_a_list(self):
         with self.assertRaises(TypeError):
-            max_integer(["hello", 1, 2])
+            max_integer([0, '1'])
+
+    def test_tuple_in_a_list(self):
+        with self.assertRaises(TypeError):
+            max_integer([10, (20, 30)])
+
+    def test_dictionary(self):
+        with self.assertRaises(KeyError):
+            max_integer({'key1': 1, 'key2': 2})
+
+    def test_number(self):
+        with self.assertRaises(TypeError):
+            max_integer(1)
