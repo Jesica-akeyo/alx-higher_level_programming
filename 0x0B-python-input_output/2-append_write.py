29e5ddcc34c87e9dd04bc295aad0be36087081fd
Jesica-akeyo <Jessica.akeyo@gmail.com>
2022-07-05 18:11:50 +0000

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,6 +2,7 @@
 """ Module that contains a function that appends to a text file
 """
 
+
 def append_write(filename="", text=""):
     """ Function that appends to a text file
     filename: file name
