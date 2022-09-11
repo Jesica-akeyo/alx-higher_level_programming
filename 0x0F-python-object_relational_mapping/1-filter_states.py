ec153ce8814700092be75b07d232f5cc3020a5d2
Jesica-akeyo <99292017+Jesica-akeyo@users.noreply.github.com>
2022-09-11 23:57:45 +0300

    fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,26 +6,17 @@ from the database hbtn_0e_0_usa
 import sys
 import MySQLdb
 
-def main():
-    conn = MySQLdb.connect(
-                        host="localhost",
-                        port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3],
-                        charset="utf8"
-                        )
-    cur = conn.cursor()
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cur.execute(query)
-    row = cur.fetchall()
-    for r i row:
-        if r[1][0] == 'N':
-            print(r)
-    cur.close()
-    conn.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
+    states = cur.fetchall()
 
-
-if __name__ == "__main__":
-    main()
+    for state in states:
+        print(state)
