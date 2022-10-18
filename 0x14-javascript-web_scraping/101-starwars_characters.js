91d1d59333c0ea86171239dec3daf5fcb6475451
Jesica-akeyo <99292017+Jesica-akeyo@users.noreply.github.com>
2022-10-18 22:06:23 +0300

    starwars

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/';
-let id = parseInt(process.argv[2], 10);
-let characters = [];
-
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const results = resp.results;
-    if (id < 4) {
-      id += 3;
-    } else {
-      id -= 3;
-    }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === id) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+function recur (array, i) {
+  if (i >= array.length) return;
+  request.get(array[i], function (error2, response2, body2) {
+    console.log(JSON.parse(body2).name);
+    recur(array, i + 1);
+  });
+}
+request.get(url, function (error, response, body) {
+  if (error) console.log(error);
+  else {
+    recur(JSON.parse(body).characters, 0);
   }
 });
