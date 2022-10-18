d5c22aa0a320f0dccae7c37925e0b31c715e2060
Jesica-akeyo <jessica.akeyo@gmail.com>
2022-10-18 19:31:45 +0000

    request store

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,7 +2,6 @@
 const request = require('request');
 const fs = require('fs');
 request.get(process.argv[2], function (error, response, body) {
-  if (error = null) {
-    fs.writeFileSync(process.argv[3], body);
-  }
+  if (error) console.log(error);
+  else if (response.statusCode === 200) fs.writeFile(process.argv[3], body, 'utf8', (err) => { if (err) throw err; });
 });
